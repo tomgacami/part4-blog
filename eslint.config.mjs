655c9de679ca2a451e79657c2ecfb69aee1d85c6
tom <tomgacami@gmail.com>
2026-04-30 18:09:26 -0300

    Making pretty eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,9 @@ export default defineConfig([
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js, stylistic },
     extends: ["js/recommended"],
-    languageOptions: { globals: globals.node }
+    languageOptions: {
+      globals: globals.node
+      }
   },
   {
     rules:{
