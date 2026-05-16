13e96ea01a9b686e7984783b8f64d89962f1aa64
tom <tomgacami@gmail.com>
2026-05-16 20:06:16 -0300

    Exercise 4.19 step 7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+
+    const authorization =  request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 
 
 blogsRouter.get('/', async (request, response) => {
@@ -14,13 +24,18 @@ blogsRouter.post('/', async(request, response) => {
 
     const body = request.body
 
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+
+    if (!decodedToken.id){
+        return response.status(401).json({ error: 'token invalid' })
+    }
+
     if (!body.title || !body.author || !body.url ){
         //ALREADY DONE IN THE NEXT LINE
         return response.status(400).json({ error: 'All fields must be fulled' })
     }
 
-    const users = await User.find({})
-    const user = users[0]
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
         title: body.title,
