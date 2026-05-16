aa06844710bed6dfd948c592f05ef490b78deece
tom <tomgacami@gmail.com>
2026-05-16 14:42:06 -0300

    Exercise 4.17 step 5

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,11 +1,12 @@
 
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 
 blogsRouter.get('/', async (request, response) => {
 
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
     response.json(blogs)
 })
 
@@ -18,14 +19,20 @@ blogsRouter.post('/', async(request, response) => {
         return response.status(400).json({ error: 'All fields must be fulled' })
     }
 
+    const users = await User.find({})
+    const user = users[0]
+
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: 0
+        likes: 0,
+        user: user.id
     })
 
     const newBlogSaved = await blog.save()
+    user.blogs = user.blogs.concat(newBlogSaved._id)
+    await user.save()
     response.status(201).json(newBlogSaved)
 })
 
